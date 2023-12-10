12f19308ca05fa0e31891657036d24c09e969dc7
gorrrik92 <gorrrik@gmail.com>
2023-12-10 23:35:13 +0500

    Lesson 32: task1 and task2

--- src/com/walking/lesson32_files_1/task1/Main.java
+++ src/com/walking/lesson32_files_1/task1/Main.java
@@ -1,11 +1,36 @@
 package com.walking.lesson32_files_1.task1;
 
+import com.walking.lesson31_io_streams.model.Car;
+
+import java.io.FileOutputStream;
+
 /**
  * Используя класс Car (или создав новый класс для сущности «машина», на ваше усмотрение) из задачи
  * <a href="https://github.com/KFalcon2022/practical-tasks/tree/master/src/com/walking/lesson19_object_methods/model">...</a>
  * Реализуйте сохранение массива машин в файл carCatalog.txt
  */
 public class Main {
+    public static final String FILE_PATH = "./resource/files/lesson32/cars.txt";
+    public static final String FIELD_DELIMETER = "_";
+    private static Car[] cars = new Car[3];
+    static {
+        cars[0] = new Car("111", 2017, "blue", true);
+        cars[1] = new Car("222", 2018, "green", true);
+        cars[2] = new Car("333", 2000, "red", false);
+    }
     public static void main(String[] args) {
+        try(FileOutputStream fout=new FileOutputStream(FILE_PATH))
+        {
+            for(Car car : cars) {
+                fout.write(carToString(car));
+                System.out.println(car.toString());
+            }
+        }  catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    private static byte[] carToString(Car car) {
+        return String.format("%s%s%d%s%s%s%b\n", car.getNumber(), FIELD_DELIMETER, car.getYear(), FIELD_DELIMETER, car.getColor(), FIELD_DELIMETER, car.isActualTechnicalInspection()).getBytes();
     }
 }
--- src/com/walking/lesson32_files_1/task2/Main.java
+++ src/com/walking/lesson32_files_1/task2/Main.java
@@ -1,10 +1,49 @@
 package com.walking.lesson32_files_1.task2;
 
+import com.walking.lesson31_io_streams.model.Car;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.IntSummaryStatistics;
+
 /**
  * Используя Задачу 1, реализуйте чтение из carCatalog.txt,
  * реализовав сохранение данных в массив Car.
  */
 public class Main {
+    public static final String FILE_PATH = "./resource/files/lesson32/cars.txt";
+    public static final String FIELD_DELIMETER = "_";
+
     public static void main(String[] args) {
+        try(FileInputStream fin=new FileInputStream(FILE_PATH))
+        {
+            StringBuilder strLine= new StringBuilder();
+            int i;
+            while((i=fin.read())!=-1){
+                char nextChar= (char) i;
+                if (nextChar == '\n') {
+                    Car car = parseCarLine(strLine.toString());
+                    System.out.println(car);
+                    strLine= new StringBuilder();
+                } else {
+                    strLine.append(nextChar);
+                }
+            }
+        }  catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    private static Car parseCarLine(String carLine) throws IllegalArgumentException {
+        String[] elements = carLine.split(FIELD_DELIMETER);
+        if (elements.length == 4) {
+            String number = elements[0];
+            Integer year = Integer.parseInt(elements[1]);
+            String color = elements[2];
+            Boolean isActual = Boolean.parseBoolean(elements[3]);
+            return new Car(number, year, color, isActual);
+        } else {
+            throw new IllegalArgumentException(String.format("Incorrect format of Car line: %s", carLine));
+        }
     }
 }
