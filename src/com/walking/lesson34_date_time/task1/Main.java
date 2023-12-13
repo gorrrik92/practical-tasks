33bfe0dcf6fe01e02689f10f525471dfa136e142
gorrrik92 <gorrrik@gmail.com>
2023-12-14 01:04:14 +0500

    Lesson 34: task1

--- src/com/walking/lesson34_date_time/task1/Main.java
+++ src/com/walking/lesson34_date_time/task1/Main.java
@@ -1,10 +1,28 @@
 package com.walking.lesson34_date_time.task1;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
 /**
  * Реализуйте метод, возвращающий объект LocalDateTime на основании переданной строки.
  * Пример входящей строки: 20.12.2022 20:16:00.
  */
 public class Main {
+    private static final String DATE_FORMATTER = "dd.MM.yyyy HH:mm:ss";
+
     public static void main(String[] args) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMATTER);
+        System.out.printf("Enter the date in the format %s:\n", DATE_FORMATTER);
+        Scanner sc = new Scanner(System.in);
+        String dateString = sc.next();
+
+        LocalDateTime parsedDate = parsedDate(dateString, formatter);
+        System.out.println(parsedDate);
+    }
+
+    private static LocalDateTime parsedDate(String str, DateTimeFormatter formatter) {
+        return LocalDateTime.parse(str, formatter);
     }
 }
