d09776a2741d94a06c1bf7d7517049c7ae335db9
gorrrik92 <gorrrik@gmail.com>
2023-12-24 00:27:00 +0500

    Lesson 41: task2

--- src/com/walking/lesson41_set/task2/Main.java
+++ src/com/walking/lesson41_set/task2/Main.java
@@ -1,5 +1,10 @@
 package com.walking.lesson41_set.task2;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
 /**
  * Реализуйте задачу
  * <a href="https://github.com/KFalcon2022/practical-tasks/tree/master/src/com/walking/lesson26_string_types/task2">...</a>,
@@ -7,5 +12,18 @@ package com.walking.lesson41_set.task2;
  */
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter words: ");
+        String[] words = scanner.nextLine()
+                .trim() //обрезаем пробелы в начале и коне введенной строки
+                .toLowerCase() //приводим все к нижнему регистру, чтобы слова в разном регистре не посчитались по отдельности
+                .split(" "); //разбиваем строку на массив, разделенный по " "
+
+        scanner.close();
+
+        Set<String> uniqueWords = new HashSet<>(Arrays.asList(words));
+
+        System.out.printf("Found %d unique words\n", uniqueWords.size());
     }
 }
