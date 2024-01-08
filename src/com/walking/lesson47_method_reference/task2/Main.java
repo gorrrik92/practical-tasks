6b1b0f3c72f836461e882ad0a0224a5a39a66e43
gorrrik92 <gorrrik@gmail.com>
2024-01-08 22:36:06 +0500

    Lesson 47: task2

--- src/com/walking/lesson47_method_reference/task2/Main.java
+++ src/com/walking/lesson47_method_reference/task2/Main.java
@@ -1,10 +1,35 @@
 package com.walking.lesson47_method_reference.task2;
 
+import com.walking.lesson47_method_reference.task1.model.Car;
+
+import java.util.List;
+
 /**
  * Реализуйте Задачу 1, обернув метод выведения записи в консоль
  * (System.out.println()) в собственный статический метод.
  */
 public class Main {
     public static void main(String[] args) {
+        List<Car> cars = initCars();
+        cars.forEach(Main::printCar);
+    }
+
+    private static List<Car> initCars() {
+        return List.of(
+                new Car("RR-111-RR", 2005, "yellow", true),
+                new Car("RR-111-RR", 2015, "yellow", true),
+                new Car("RR-222-RR", 2016, "silver", true),
+                new Car("RR-333-RR", 2017, "blue", true),
+                new Car("RR-444-RR", 2018, "yellow", true),
+                new Car("RR-555-RR", 2018, "black", true),
+                new Car("RR-666-RR", 2018, "yellow", true),
+                new Car("RR-777-RR", 2020, "yellow", true),
+                new Car("RR-888-RR", 2018, "green", true),
+                new Car("RR-999-RR", 2008, "white", true),
+                new Car("RR-000-RR", 2018, "yellow", true));
+    }
+
+    private static void printCar(Car car) {
+        System.out.println(car);
     }
 }
---
+++ src/com/walking/lesson47_method_reference/task2/model/Car.java
@@ -0,0 +1,48 @@
+package com.walking.lesson47_method_reference.task2.model;
+
+import com.walking.lesson46_lambda.task3.model.CarIdentifier;
+
+public class Car {
+    //    Уникальным идентификатором будет объект immutable класса CarIdentifier
+    private final CarIdentifier identifier;
+
+    private String color;
+    private boolean actualTechnicalInspection;
+
+    public Car(String number, int year, String color, boolean actualTechnicalInspection) {
+        this(new CarIdentifier(number, year), color, actualTechnicalInspection);
+    }
+
+    public Car(CarIdentifier identifier, String color, boolean actualTechnicalInspection) {
+        this.identifier = identifier;
+        this.color = color;
+        this.actualTechnicalInspection = actualTechnicalInspection;
+    }
+
+    public CarIdentifier getIdentifier() {
+        return identifier;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isActualTechnicalInspection() {
+        return actualTechnicalInspection;
+    }
+
+    public void setActualTechnicalInspection(boolean actualTechnicalInspection) {
+        this.actualTechnicalInspection = actualTechnicalInspection;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("number: %s year: %d color: %s actualTechnicalInspection: %s",
+                identifier.getNumber(), identifier.getYear(), color,
+                actualTechnicalInspection ? "actual" : "not actual");
+    }
+}
