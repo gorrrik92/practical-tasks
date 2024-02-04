a98a52ca2c492841ab5a698d3e57b8ab4a280f12
gorrrik92 <gorrrik@gmail.com>
2024-02-04 17:36:08 +0500

    Lesson 57 task 23-24

--- src/com/walking/lesson57_stream_collect_collector/task/Task23.java
+++ src/com/walking/lesson57_stream_collect_collector/task/Task23.java
@@ -1,8 +1,11 @@
 package com.walking.lesson57_stream_collect_collector.task;
 
 import com.walking.lesson57_stream_collect_collector.model.Department;
+import com.walking.lesson57_stream_collect_collector.model.Employee;
 
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Предоставьте информацию, превышает ли численность мужчин в компании численность женщин.
@@ -10,6 +13,10 @@ import java.util.List;
 public class Task23 implements StatisticTask<Boolean> {
     @Override
     public Boolean calculate(List<Department> departments) {
-        return null;
+        return departments.stream()
+                .map(Department::getEmployees)
+                .flatMap(Collection::stream)
+                .collect(Collectors.collectingAndThen(Collectors.partitioningBy(Employee::isMale),
+                        map -> map.get(true).size() > map.get(false).size()));
     }
 }
--- src/com/walking/lesson57_stream_collect_collector/task/Task24.java
+++ src/com/walking/lesson57_stream_collect_collector/task/Task24.java
@@ -1,9 +1,12 @@
 package com.walking.lesson57_stream_collect_collector.task;
 
 import com.walking.lesson57_stream_collect_collector.model.Department;
+import com.walking.lesson57_stream_collect_collector.model.Employee;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Предоставьте информацию, превышает ли численность мужчин численность женщин по каждому департаменту.
@@ -11,6 +14,10 @@ import java.util.Map;
 public class Task24 implements StatisticTask<Map<String, Boolean>> {
     @Override
     public Map<String, Boolean> calculate(List<Department> departments) {
-        return null;
+        return departments.stream()
+                .collect(Collectors.groupingBy(Department::getName,
+                        Collectors.flatMapping(department -> department.getEmployees().stream(),
+                                Collectors.collectingAndThen(Collectors.partitioningBy(Employee::isMale),
+                                        map -> map.get(true).size() > map.get(false).size()))));
     }
 }
